b343414c777d296cbb583c81194cba71222e5909
Jocomol <schoeggu8@gmail.com>
2021-10-04 16:43:52 +0200

    added an fixme

--- elevator/controlpanel.py
+++ elevator/controlpanel.py
@@ -50,7 +50,7 @@ def main():
         story_dir = abspath(story).strip("/" + story)
         sys.path.append(story_dir)
         story_class_file = abspath(story).split("/")[len(abspath(story).split("/")) - 1]
-        story_class = importlib.import_module(story_class_file)
+        story_class = importlib.import_module(story_class_file)  # FIXME
         story_object = story_class.Story()
         exitCode = story_object.test()
         if exitCode > 0:
