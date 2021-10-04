21ea1eeaa60c463da2507496f853e4b554d297f2
Jocomol <schoeggu8@gmail.com>
2021-10-04 08:23:30 +0200

    added the default keywords as a classvariable to additionalActions so we don't have to create a new DefaultActions object everytime we use a new keywordpackage

--- actions/additionalActions.py
+++ actions/additionalActions.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3 -B
+import actions.defaultActions
+
 
 class AdditionalActions:
 
     browser = None
 
-    def __init__(self, browser):
+    def __init__(self, browser, default):
         self.browser = browser
+        self.default = default
 
